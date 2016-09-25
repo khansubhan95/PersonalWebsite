008abdcc55a8894c88c715fa8cad99dc1f32b5d9
subhan <khansubhan20@yahoo.com>
2016-09-25 15:36:26 -0400

    jQuery plugin added,prettied some html,changed models to add slug in models projects ,custom.css edited,projects.html edited to reflect slug,urls.py of projects edited to include slug,ssl certificate added

--- projects/models.py
+++ projects/models.py
@@ -8,6 +8,7 @@ class Project(models.Model):
 	title=models.CharField(max_length=140)
 	body=models.TextField()
 	date=models.DateTimeField()
+	slug=models.SlugField(max_length=60,blank=True)
 
 	def __str__(self):
 		return self.title
