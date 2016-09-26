933af4d963938bc5b2f6331b383b6731b75d89a3
subhan <khansubhan20@yahoo.com>
2016-09-26 08:37:18 -0400

    Added url for sitemap to mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import home,contact,about_website
+from .views import home,contact,about_website,site_map
 
 urlpatterns=[
 	url(r'^$',home,name='home'),
 	url(r'^contact/$',contact,name='contact'),
 	url(r'^about/website/$',about_website,name='about_website'),
+	url(r'^sitemap.xml/$',site_map,name='site_map'),
 ]
