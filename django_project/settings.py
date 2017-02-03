2a4dcc7182915c3b76220d28edd0b8d9ebefe10e
subhan <khansubhan20@yahoo.com>
2017-02-03 14:33:17 -0500

    Remove secrets from settings.py to .env
    Application settings like api keys and passwords were a part of the
    settings.py file. Installing a package python-decouple allowed moving
    the secrets to a .env located in project root. Refernce the settings
    using config()

---
+++ django_project/settings.py
@@ -0,0 +1,102 @@
+"""
+Django settings for django_project project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+from decouple import config, Csv
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', cast=bool) 
+
+TEMPLATE_DEBUG = config('TEMPLATE_DEBUG', cast=bool) 
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = ['ip_address_given_above']
+# Used when no domain name is available
+
+# Application definition
+
+INSTALLED_APPS = (
+    'mysite',
+    'projects',
+    'posts',
+    'django_markdown',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+
+ROOT_URLCONF = 'django_project.urls'
+
+WSGI_APPLICATION = 'django_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME':  config('DB_NAME'),
+	'USER': config('DB_USER'),
+	'PASSWORD': config('DB_PASSWORD'),
+	'HOST': config('DB_HOST'),
+	'PORT': '',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Kolkata'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = config('STATIC_ROOT')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = config('MEDIA_ROOT')
+MEDIA_URL = '/media/'
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool) 
