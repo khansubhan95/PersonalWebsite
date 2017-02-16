67b4f4a47769f03fa4b0b87bf27291cc1b9644a2
subhan <khansubhan20@yahoo.com>
2017-02-16 11:18:32 -0500

    Add new model configuration to projects/urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,7 @@ from .models import Project
 # or from projects.models import Project
 
 urlpatterns=[
-	url(r'^$',ListView.as_view(queryset=Project.objects.all().order_by("-date")[:25],template_name=\
+	url(r'^$',ListView.as_view(queryset=Project.objects.filter(published=True).order_by("-date")[:25],template_name=\
 	'projects/projects.html')),
 	# url(r'^(?P<pk>\d+)/$',DetailView.as_view(model=Project,template_name='projects/project.html')),
 	url(r'^(?P<slug>[a-zA-Z0-9_-]+)/$',DetailView.as_view(model=Project,template_name='projects/project.html')),
