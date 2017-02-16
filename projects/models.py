689712d937986f5efa79e3132698fe79d0019449
subhan <khansubhan20@yahoo.com>
2017-02-16 11:05:46 -0500

    Change post model
    Added a boolean field to specify whether the post should be published or
    not.

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        return super(PostManager, self).filter(published=True)
 
 class Project(models.Model):
 	title=models.CharField(max_length=140)
@@ -10,6 +13,9 @@ class Project(models.Model):
 	body=models.TextField()
 	date=models.DateTimeField()
 	slug=models.SlugField(max_length=60,blank=True)
+        published = models.BooleanField(default=True)
+
+        objects = PostManager()
 
 	def __str__(self):
 		return self.title
